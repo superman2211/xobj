0a6278cb58d4b4dcad948dd85815376fe3f4d3c2
Sergey Antonov <superflasher22@gmail.com>
2022-02-01 19:06:44 +0300

    Clean

--- packages/core/src/decoders/boolean.ts
+++ packages/core/src/decoders/boolean.ts
@@ -1,14 +1,7 @@
 import { ValueType } from '../types';
-import { DecoderMethod, DecodeState } from '../decode';
-
-export function decodeBitset(state: DecodeState): boolean[] {
-	const { reader } = state;
-	const count = reader.readUintVar();
-	return reader.readBitset(count);
-}
+import { DecoderMethod } from '../decode';
 
 export function initBooleanDecoders(decoders: Map<ValueType, DecoderMethod>) {
 	decoders.set(ValueType.FALSE, () => false);
 	decoders.set(ValueType.TRUE, () => true);
-	decoders.set(ValueType.BITSET, decodeBitset);
 }
--- packages/core/src/encoders/boolean.ts
+++ packages/core/src/encoders/boolean.ts
@@ -9,14 +9,7 @@ export function detectBoolean(state: EncodeState, value: any): ValueType {
 	return ValueType.UNKNOWN;
 }
 
-export function encodeBitset(state: EncodeState, value: boolean[]) {
-	const { writer } = state;
-	writer.writeUintVar(value.length);
-	writer.writeBitset(value);
-}
-
 export function initBooleanEncoders(encoders: Map<ValueType, EncoderMethod>) {
 	encoders.set(ValueType.TRUE, () => { });
 	encoders.set(ValueType.FALSE, () => { });
-	encoders.set(ValueType.BITSET, encodeBitset);
 }
