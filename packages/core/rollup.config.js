6f9cc758f5060c91ab061eaedfcbb053ecf85815
Sergey Antonov <superflasher22@gmail.com>
2022-03-19 23:49:30 +0300

    Fix rollup config

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -2,27 +2,33 @@ import sourcemaps from 'rollup-plugin-sourcemaps';
 import { terser } from 'rollup-plugin-terser';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-export default {
-	input: 'dist/esm/index.js',
-	output: [
-		{
+export default [
+	{
+		input: 'dist/esm/index.js',
+		output: {
 			file: 'dist/cjs/index.js',
 			format: 'cjs',
 			exports: 'auto',
 			sourcemap: 'inline',
-			plugins: [sourcemaps()],
 		},
-		{
+		external: ['@xobj/buffer'],
+		plugins: [
+			sourcemaps(),
+		],
+	},
+	{
+		input: 'dist/esm/index.js',
+		output: {
 			file: 'dist/iife/xobj-core.min.js',
 			name: 'xobjCore',
 			format: 'iife',
 			compact: true,
 			sourcemap: true,
-			plugins: [
-				nodeResolve(),
-				sourcemaps(),
-				terser(),
-			],
 		},
-	],
-};
+		plugins: [
+			nodeResolve(),
+			sourcemaps(),
+			terser(),
+		],
+	},
+];
