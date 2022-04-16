d6b79ede435f8dd14c1e72df1cebd22efd883cc1
Sergey Antonov <superflasher22@gmail.com>
2022-04-16 23:59:59 +0300

    Update sample

--- samples/browser-sample/scripts/build.js
+++ samples/browser-sample/scripts/build.js
@@ -7,7 +7,8 @@ if (!fs.existsSync('dist')) {
 fs.copyFileSync('src/index.html', 'dist/index.html');
 fs.copyFileSync('src/index.js', 'dist/index.js');
 
-fs.copyFileSync(require.resolve('@xobj/core/dist/iife/xobj-core.min.js'), 'dist/xobj-core.min.js');
-fs.copyFileSync(require.resolve('@xobj/core/dist/iife/xobj-core.min.js.map'), 'dist/xobj-core.min.js.map');
+fs.copyFileSync(require.resolve('@xobj/core/dist/iife/xobj-core.es6.min.js'), 'dist/xobj-core.es6.min.js');
+fs.copyFileSync(require.resolve('@xobj/core/dist/iife/xobj-core.es6.min.js.map'), 'dist/xobj-core.es6.min.js.map');
 
+// eslint-disable-next-line no-console
 console.log('Build completed');
