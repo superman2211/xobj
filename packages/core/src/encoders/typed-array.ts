84d9734bfd630aa61cc231abbd814af33dd31688
Sergey Antonov <superflasher22@gmail.com>
2022-04-23 17:32:19 +0300

    Add typed array tests

--- packages/core/src/encoders/typed-array.ts
+++ packages/core/src/encoders/typed-array.ts
@@ -1,5 +1,5 @@
 import { DetectorMethod, EncoderMethod, EncodeState } from '../encode';
-import { ValueType } from '../types';
+import { TypedArrayType, ValueType } from '../types';
 
 export function detectTypedArray(state: EncodeState, value: any): ValueType {
 	if (value instanceof Uint8ClampedArray) {
@@ -49,43 +49,43 @@ export function encodeTypedArray(state: EncodeState, value: any) {
 	const { writer } = state;
 
 	if (value instanceof Uint8ClampedArray) {
-		writer.writeUint8(ValueType.UINT8);
+		writer.writeUint8(TypedArrayType.UINT8_CLAMPED);
 	}
 
 	if (value instanceof Uint8Array) {
-		writer.writeUint8(ValueType.UINT8);
+		writer.writeUint8(TypedArrayType.UINT8);
 	}
 
 	if (value instanceof Uint16Array) {
-		writer.writeUint8(ValueType.UINT16);
+		writer.writeUint8(TypedArrayType.UINT16);
 	}
 
 	if (value instanceof Uint32Array) {
-		writer.writeUint8(ValueType.UINT32);
+		writer.writeUint8(TypedArrayType.UINT32);
 	}
 
 	if (value instanceof Int8Array) {
-		writer.writeUint8(ValueType.INT8);
+		writer.writeUint8(TypedArrayType.INT8);
 	}
 
 	if (value instanceof Int16Array) {
-		writer.writeUint8(ValueType.INT16);
+		writer.writeUint8(TypedArrayType.INT16);
 	}
 
 	if (value instanceof Int32Array) {
-		writer.writeUint8(ValueType.INT32);
+		writer.writeUint8(TypedArrayType.INT32);
 	}
 
 	if (value instanceof Float32Array) {
-		writer.writeUint8(ValueType.FLOAT32);
+		writer.writeUint8(TypedArrayType.FLOAT32);
 	}
 
 	if (value instanceof Float64Array) {
-		writer.writeUint8(ValueType.FLOAT64);
+		writer.writeUint8(TypedArrayType.FLOAT64);
 	}
 
 	if (value instanceof DataView) {
-		writer.writeUint8(ValueType.ANY);
+		writer.writeUint8(TypedArrayType.DATA_VIEW);
 	}
 
 	const buffer: ArrayBuffer = value.buffer.slice(value.byteOffset, value.byteLength);
