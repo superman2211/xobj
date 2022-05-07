d94abe3fe96576ea21fd7b92d9f70134880ade9e
Sergey Antonov <superflasher22@gmail.com>
2022-05-07 18:05:34 +0300

    Correct tests

--- packages/buffer/test/number.test.ts
+++ packages/buffer/test/number.test.ts
@@ -8,48 +8,72 @@ import { BufferWriter } from '../src/writer';
 type ReaderClass = new (...args: any[]) => IBufferReader;
 type WriterClass = new (...args: any[]) => IBufferWriter;
 
-function group(name: string, Reader: ReaderClass, Writer: WriterClass, littleEndian: boolean) {
-	describe(name, () => {
-		it('should write numbers correctly', () => {
-			const writer = new Writer(1024, littleEndian);
-			writer.writeInt8(123);
-			writer.writeInt8(-45);
-			writer.writeInt16(10456);
-			writer.writeInt16(-1000);
-			writer.writeInt32(78901);
-			writer.writeInt32(-6376247);
-			writer.writeUint8(245);
-			writer.writeUint16(2048);
-			writer.writeUint32(0xabcdef);
-			writer.writeFloat32(123.456);
-			writer.writeFloat64(1234.56789);
-			expect(writer.length).toBe(33);
-			expect(writer.position).toBe(33);
+function act(Reader: ReaderClass, Writer: WriterClass, littleEndian: boolean) {
+	it('should write numbers correctly', () => {
+		const writer = new Writer(1024, littleEndian);
+		writer.writeInt8(123);
+		writer.writeInt8(-45);
+		writer.writeInt16(10456);
+		writer.writeInt16(-1000);
+		writer.writeInt32(78901);
+		writer.writeInt32(-6376247);
+		writer.writeUint8(245);
+		writer.writeUint16(2048);
+		writer.writeUint32(0xabcdef);
+		writer.writeFloat32(123.456);
+		writer.writeFloat64(1234.56789);
+		expect(writer.length).toBe(33);
+		expect(writer.position).toBe(33);
 
-			const reader = new Reader(writer.buffer, littleEndian);
-			expect(reader.length).toBe(33);
-			expect(reader.readInt8()).toBe(123);
-			expect(reader.readInt8()).toBe(-45);
-			expect(reader.readInt16()).toBe(10456);
-			expect(reader.readInt16()).toBe(-1000);
-			expect(reader.readInt32()).toBe(78901);
-			expect(reader.readInt32()).toBe(-6376247);
-			expect(reader.readUint8()).toBe(245);
-			expect(reader.readUint16()).toBe(2048);
-			expect(reader.readUint32()).toBe(0xabcdef);
-			expect(reader.readFloat32()).toBeCloseTo(123.456, 3);
-			expect(reader.readFloat64()).toBeCloseTo(1234.56789, 5);
-			expect(reader.position).toBe(33);
-		});
+		const reader = new Reader(writer.buffer, littleEndian);
+		expect(reader.length).toBe(33);
+		expect(reader.readInt8()).toBe(123);
+		expect(reader.readInt8()).toBe(-45);
+		expect(reader.readInt16()).toBe(10456);
+		expect(reader.readInt16()).toBe(-1000);
+		expect(reader.readInt32()).toBe(78901);
+		expect(reader.readInt32()).toBe(-6376247);
+		expect(reader.readUint8()).toBe(245);
+		expect(reader.readUint16()).toBe(2048);
+		expect(reader.readUint32()).toBe(0xabcdef);
+		expect(reader.readFloat32()).toBeCloseTo(123.456, 3);
+		expect(reader.readFloat64()).toBeCloseTo(1234.56789, 5);
+		expect(reader.position).toBe(33);
 	});
 }
 
-group('number normal+normal big endian', BufferReader, BufferWriter, false);
-group('number normal+fast big endian', BufferReader, FastBufferWriter, false);
-group('number fast+normal big endian', FastBufferReader, BufferWriter, false);
-group('number fast+fast big endian', FastBufferReader, FastBufferWriter, false);
+describe('number big endian', () => {
+	describe('BufferReader & BufferWriter', () => {
+		act(BufferReader, BufferWriter, false);
+	});
 
-group('number normal+normal', BufferReader, BufferWriter, true);
-group('number normal+fast', BufferReader, FastBufferWriter, true);
-group('number fast+normal', FastBufferReader, BufferWriter, true);
-group('number fast+fast', FastBufferReader, FastBufferWriter, true);
+	describe('FastBufferReader & BufferWriter', () => {
+		act(FastBufferReader, BufferWriter, false);
+	});
+
+	describe('BufferReader & FastBufferWriter', () => {
+		act(BufferReader, FastBufferWriter, false);
+	});
+
+	describe('FastBufferReader & FastBufferWriter', () => {
+		act(FastBufferReader, FastBufferWriter, false);
+	});
+});
+
+describe('number little endian', () => {
+	describe('BufferReader & BufferWriter', () => {
+		act(BufferReader, BufferWriter, true);
+	});
+
+	describe('FastBufferReader & BufferWriter', () => {
+		act(FastBufferReader, BufferWriter, true);
+	});
+
+	describe('BufferReader & FastBufferWriter', () => {
+		act(BufferReader, FastBufferWriter, true);
+	});
+
+	describe('FastBufferReader & FastBufferWriter', () => {
+		act(FastBufferReader, FastBufferWriter, true);
+	});
+});
--- packages/buffer/test/stream.test.ts
+++ packages/buffer/test/stream.test.ts
@@ -1,141 +1,145 @@
 /* eslint-disable no-undef */
+import { IBufferReader, IBufferWriter } from '../src/types';
 import { BufferReader } from '../src/reader';
 import { BufferWriter } from '../src/writer';
 import { FastBufferWriter } from '../src/fast-writer';
-import { IBufferReader, IBufferWriter } from '../src/types';
+import { FastBufferReader } from '../src/fast-reader';
 
 type ReaderClass = new (...args: any[]) => IBufferReader;
 type WriterClass = new (...args: any[]) => IBufferWriter;
 
-function group(name: string, Reader: ReaderClass, Writer: WriterClass) {
-	describe(name, () => {
-		it('should get length of empty stream', () => {
-			const writer = new Writer();
-			expect(writer.length).toBe(0);
-		});
+function act(Reader: ReaderClass, Writer: WriterClass) {
+	it('should get length of empty stream', () => {
+		const writer = new Writer();
+		expect(writer.length).toBe(0);
+	});
 
-		it('should get correct length and position', () => {
-			const writer = new Writer();
-			writer.writeInt8(123);
-			expect(writer.position).toBe(1);
-			writer.writeUint16(2048);
-			expect(writer.position).toBe(3);
-			writer.writeUint32(123456789);
-			expect(writer.position).toBe(7);
-			writer.position = 1;
-			writer.writeInt16(-2048);
-			expect(writer.position).toBe(3);
-			expect(writer.length).toBe(7);
+	it('should get correct length and position', () => {
+		const writer = new Writer();
+		writer.writeInt8(123);
+		expect(writer.position).toBe(1);
+		writer.writeUint16(2048);
+		expect(writer.position).toBe(3);
+		writer.writeUint32(123456789);
+		expect(writer.position).toBe(7);
+		writer.position = 1;
+		writer.writeInt16(-2048);
+		expect(writer.position).toBe(3);
+		expect(writer.length).toBe(7);
 
-			const { buffer } = writer;
+		const { buffer } = writer;
 
-			const reader = new Reader(buffer);
-			expect(reader.length).toBe(7);
-			expect(reader.buffer).toBe(buffer);
-			expect(reader.buffer.byteLength).toBe(7);
-			expect(reader.readInt8()).toBe(123);
-			expect(reader.position).toBe(1);
-			expect(reader.readInt16()).toBe(-2048);
-			expect(reader.position).toBe(3);
-			expect(reader.readUint32()).toBe(123456789);
-			expect(reader.position).toBe(7);
-			reader.position = 1;
-			expect(reader.readInt16()).toBe(-2048);
-			expect(reader.position).toBe(3);
-		});
+		const reader = new Reader(buffer);
+		expect(reader.length).toBe(7);
+		expect(reader.buffer).toBe(buffer);
+		expect(reader.buffer.byteLength).toBe(7);
+		expect(reader.readInt8()).toBe(123);
+		expect(reader.position).toBe(1);
+		expect(reader.readInt16()).toBe(-2048);
+		expect(reader.position).toBe(3);
+		expect(reader.readUint32()).toBe(123456789);
+		expect(reader.position).toBe(7);
+		reader.position = 1;
+		expect(reader.readInt16()).toBe(-2048);
+		expect(reader.position).toBe(3);
+	});
 
-		it('should get correct length after allocate', () => {
-			const writer = new Writer();
-			let i = 3000;
-			while (i--) {
-				writer.writeUint8(1);
-			}
-			expect(writer.length).toBe(3000);
-			expect(writer.bufferSize).toBe(4096);
-		});
+	it('should get correct length after allocate', () => {
+		const writer = new Writer();
+		let i = 3000;
+		while (i--) {
+			writer.writeUint8(1);
+		}
+		expect(writer.length).toBe(3000);
+		expect(writer.bufferSize).toBe(4096);
+	});
 
-		it('should write data in big endian', () => {
-			const writer = new Writer(1024, false);
-			writer.writeInt32(78901);
-			expect(writer.littleEndian).toBe(false);
+	it('should write data in big endian', () => {
+		const writer = new Writer(1024, false);
+		writer.writeInt32(78901);
+		expect(writer.littleEndian).toBe(false);
 
-			const { buffer } = writer;
-			const array = new Uint8Array(buffer);
+		const { buffer } = writer;
+		const array = new Uint8Array(buffer);
 
-			expect(array[0]).toBe(0);
-			expect(array[1]).toBe(1);
-			expect(array[2]).toBe(52);
-			expect(array[3]).toBe(53);
-			const reader = new Reader(buffer, false);
-			expect(reader.readInt32()).toBe(78901);
-			expect(reader.littleEndian).toBe(false);
-		});
+		expect(array[0]).toBe(0);
+		expect(array[1]).toBe(1);
+		expect(array[2]).toBe(52);
+		expect(array[3]).toBe(53);
+		const reader = new Reader(buffer, false);
+		expect(reader.readInt32()).toBe(78901);
+		expect(reader.littleEndian).toBe(false);
+	});
 
-		it('should write data in little endian', () => {
-			const writer = new Writer(1024, true);
-			writer.writeInt32(78901);
-			expect(writer.littleEndian).toBe(true);
+	it('should write data in little endian', () => {
+		const writer = new Writer(1024, true);
+		writer.writeInt32(78901);
+		expect(writer.littleEndian).toBe(true);
 
-			const { buffer } = writer;
-			const array = new Uint8Array(buffer);
+		const { buffer } = writer;
+		const array = new Uint8Array(buffer);
 
-			expect(array[0]).toBe(53);
-			expect(array[1]).toBe(52);
-			expect(array[2]).toBe(1);
-			expect(array[3]).toBe(0);
+		expect(array[0]).toBe(53);
+		expect(array[1]).toBe(52);
+		expect(array[2]).toBe(1);
+		expect(array[3]).toBe(0);
 
-			const reader = new Reader(buffer, true);
-			expect(reader.bytesAvailable).toBe(4);
-			expect(reader.readInt32()).toBe(78901);
-			expect(reader.bytesAvailable).toBe(0);
-			expect(reader.littleEndian).toBe(true);
-		});
+		const reader = new Reader(buffer, true);
+		expect(reader.bytesAvailable).toBe(4);
+		expect(reader.readInt32()).toBe(78901);
+		expect(reader.bytesAvailable).toBe(0);
+		expect(reader.littleEndian).toBe(true);
+	});
 
-		it('should change position', () => {
-			const writer = new Writer(1024, false);
-			expect(writer.position).toBe(0);
-			writer.writeInt32(78901);
-			expect(writer.position).toBe(4);
-			writer.position = 2;
-			expect(writer.bytesAvailable).toBe(2);
-			expect(writer.position).toBe(2);
+	it('should change position', () => {
+		const writer = new Writer(1024, false);
+		expect(writer.position).toBe(0);
+		writer.writeInt32(78901);
+		expect(writer.position).toBe(4);
+		writer.position = 2;
+		expect(writer.bytesAvailable).toBe(2);
+		expect(writer.position).toBe(2);
 
-			const { buffer } = writer;
+		const { buffer } = writer;
 
-			const reader = new Reader(buffer, false);
-			expect(reader.position).toBe(0);
-			expect(reader.readInt32()).toBe(78901);
-			expect(reader.position).toBe(4);
-			reader.position = 3;
-			expect(reader.position).toBe(3);
-		});
+		const reader = new Reader(buffer, false);
+		expect(reader.position).toBe(0);
+		expect(reader.readInt32()).toBe(78901);
+		expect(reader.position).toBe(4);
+		reader.position = 3;
+		expect(reader.position).toBe(3);
+	});
 
-		it('should throw error when position out of range', () => {
-			const writer = new Writer(1024, false);
-			writer.writeInt32(78901);
+	it('should throw error when position out of range', () => {
+		const writer = new Writer(1024, false);
+		writer.writeInt32(78901);
 
-			expect(() => {
-				writer.position = -1;
-			}).toThrow();
+		expect(() => {
+			writer.position = -1;
+		}).toThrow();
 
-			expect(() => {
-				writer.position = 10;
-			}).toThrow();
+		expect(() => {
+			writer.position = 10;
+		}).toThrow();
 
-			const { buffer } = writer;
+		const { buffer } = writer;
 
-			const reader = new Reader(buffer, false);
+		const reader = new Reader(buffer, false);
 
-			expect(() => {
-				reader.position = -3;
-			}).toThrow();
+		expect(() => {
+			reader.position = -3;
+		}).toThrow();
 
-			expect(() => {
-				reader.position = 5;
-			}).toThrow();
-		});
+		expect(() => {
+			reader.position = 5;
+		}).toThrow();
 	});
 }
 
-group('stream', BufferReader, BufferWriter);
-group('stream fast', BufferReader, FastBufferWriter);
+describe('stream', () => {
+	act(BufferReader, BufferWriter);
+});
+
+describe('stream fast', () => {
+	act(FastBufferReader, FastBufferWriter);
+});
