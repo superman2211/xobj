aa35241a630115bb32f50cec2f15dea8c09e37d2
Sergey Antonov <superflasher22@gmail.com>
2023-01-01 22:56:39 +0300

    Detectors refactoring

--- packages/flow/src/detectors/object.ts
+++ packages/flow/src/detectors/object.ts
@@ -1,9 +1,10 @@
-/* istanbul ignore file */
 import { ValueType } from '../types';
 
 export function detectObject(value: any): ValueType {
+	/* istanbul ignore next */
 	if (typeof value === 'object') {
 		return ValueType.OBJECT;
 	}
+	/* istanbul ignore next */
 	return ValueType.UNKNOWN;
 }
